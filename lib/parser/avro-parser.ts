b0a7fce798f205d7817c85d09f291066d34214e9
charlescol <charlescol@hotmail.fr>
2024-10-09 22:52:05 -0400

    refactor: avro extract dependencies

--- lib/parser/avro-parser.ts
+++ lib/parser/avro-parser.ts
@@ -3,42 +3,36 @@ import AbstractParser from './abstract-parser';
 
 export default class AvroParser extends AbstractParser {
   protected extensions = ['.avro', '.avsc'];
+  private static readonly primitiveTypes = [
+    'null',
+    'boolean',
+    'int',
+    'long',
+    'float',
+    'double',
+    'bytes',
+    'string',
+    'record',
+    'array',
+    'enum',
+    'fixed',
+    'map',
+    'union',
+  ];
 
   protected extractDependencies(filePath: string): string[] {
     const content = fs.readFileSync(filePath, 'utf8');
-    const schema = JSON.parse(content);
     const dependencies = new Set<string>();
 
-    const primitiveTypes = ['null', 'boolean', 'int', 'long', 'float', 'double', 'bytes', 'string'];
+    const regex = /"(type|items|values)"\s*:\s*"([^"]+)"/g;
+    const matches = content.matchAll(regex);
 
-    const extractDependenciesFromField = (field: any) => {
-      if (typeof field === 'string') {
-        if (!primitiveTypes.includes(field)) {
-          dependencies.add((field as string).toLowerCase());
-        }
-      } else if (Array.isArray(field)) {
-        field.forEach((item) => extractDependenciesFromField(item));
-      } else if (field.type === 'record') {
-        const recordName = field.name as string;
-        dependencies.add(recordName.toLowerCase());
-        if (field.fields) {
-          extractDependenciesFromFields(field.fields);
-        }
-      } else if (field.type === 'array' && field.items) {
-        extractDependenciesFromField(field.items);
+    for (const match of matches) {
+      const typeFound = match[2];
+      if (!AvroParser.primitiveTypes.includes(typeFound)) {
+        dependencies.add(typeFound.split('.').pop() as string);
       }
-    };
-
-    const extractDependenciesFromFields = (fields: { type: object }[]) => {
-      fields.forEach((field) => {
-        extractDependenciesFromField(field.type);
-      });
-    };
-
-    if (schema.fields) {
-      extractDependenciesFromFields(schema.fields);
     }
-
     return Array.from(dependencies);
   }
 
